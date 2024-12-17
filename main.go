d31583ddf82925fe97b6696a2004825448b94fd1
Ramyasri <ramyakanchustambham@gmail.com>
2024-12-17 15:21:12 +0530

    changed port number to 8081

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("server is starting on port 8080")
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Println("server is starting on port 8081")
+	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
 		fmt.Println("error starting the server", err)
 	}
